101e034913fc0bd92ae91ba3685b051ae215ed94
gpqls92 <86402466+gpqls92@users.noreply.github.com>
2021-07-22 16:57:04 +0900

    update5

--- js/index.js
+++ js/index.js
@@ -5,6 +5,59 @@ window.addEventListener('DOMContentLoaded',function(){
         $('.popup').hide();
     });
 
+    /* body-3 */
+    $(function(){
+
+        $.ajax({
+            url:'./data/data.json',
+            success:function(data){
+                console.log(data)
+                let tit, subtit, link, detail,tagList ='', dix=0;
+                
+                data.founed.forEach(function(v){
+                    tagList += `<li>
+                                    <address>${v.name}<br><p>${v.subname}</p></address>
+                                    <img src="${v.photo}" alt="${v.name}">
+                                </li>`;
+                });
+                $('.body-3 ul').html(tagList);
+            
+            function dataChange(){
+                tit =data.founed[idx].name;
+                subtit = data.founed[idx].subname;
+                link = data.founed[idx].link;
+                detail = data.founed[idx].detail;
+                
+
+                tagList = `<h2>${tit} <br> [${subtit}] </h2>
+                            
+                            <div>
+                                ${detail}
+                                <a href="${link}" target="blank">
+                                    Shortcut
+                                </a>
+                            </div>`;
+                $('.popup2').html(tagList);
+            }
+
+            $('.body-3 ul li').on('click',function(){
+                idx = $(this).index();
+                dataChange();
+                $('.body-3 .popup2').css('display','block');
+            });
+
+            $('.body-3 .popup2').on('click',function(){
+                $(this).hide();
+            });
+            },
+
+            error:function(e){
+                console.log(e.status);
+                console.log(e.error);
+                console.log(e.responseText);
+            }
+        })
+    });
 
 
     /* body-4 */
