bc251aaff9559b3583ec5d114d90e28a4c4b1318
gpqls92 <86402466+gpqls92@users.noreply.github.com>
2021-07-26 18:23:05 +0900

    update7

--- js/common.js
+++ js/common.js
@@ -2,19 +2,44 @@ $(function(){
 
     $('header').load('inc.html .header > div',header);
     $('footer').load('inc.html .footer');
-    $('aside').load('inc.html .top')
+    $('aside').load('inc.html aside a,aside img')
 
 
 
     
     function header(){
     /* header */
-    const elBurger = document.querySelector('.menu-trigger');
-    const elNav = document.querySelector('.menu');
-    elBurger.addEventListener('click',function(){
-        this.classList.toggle('active-1');
-        elNav.classList.toggle('active');
-    });
+        const elBurger = document.querySelector('.menu-trigger');
+        const elNav = document.querySelector('.menu');
+        elBurger.addEventListener('click',function(){
+            this.classList.toggle('active-1');
+            elNav.classList.toggle('active');
+        });
+        sideTop()
+
+
     }
+
+
+
+    function sideTop(){
+        /* aside */
+        const elAside = document.querySelector('.top');
+        
+        window.addEventListener('scroll',function(){
+            let domHei = document.documentElement.offsetHeight;
+            let winHei = window.innerHeight;
+
+            if(domHei - winHei <= window.scrollY){
+                console.log('최하단');
+            }
+            if(window.innerHeight < window.scrollY){
+                elAside.classList.add('active');
+            }else{
+                elAside.classList.remove('active');
+            }
+        })
+    }
+    
 
 });
