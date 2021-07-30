fc073951f4bd35f46e427979eff4a7dceb6f6612
gpqls92 <86402466+gpqls92@users.noreply.github.com>
2021-07-30 12:10:00 +0900

    upload3

--- js/common.js
+++ js/common.js
@@ -19,27 +19,6 @@ $(function(){
 
 
     }
-
-
-
-    function sideTop(){
-        /* aside */
-        const elAside = document.querySelector('.top');
-        
-        window.addEventListener('scroll',function(){
-            let domHei = document.documentElement.offsetHeight;
-            let winHei = window.innerHeight;
-
-            if(domHei - winHei <= window.scrollY){
-                console.log('최하단');
-            }
-            if(window.innerHeight < window.scrollY){
-                elAside.classList.add('active');
-            }else{
-                elAside.classList.remove('active');
-            }
-        })
-    }
     
 
 });
